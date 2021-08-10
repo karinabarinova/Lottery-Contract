9b61a34e7edd14c2e3e3b2f4330aa1a2f51c2bf9
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-08-10 16:57:19 +0300

    add compile script, test deployment and entering the lottery

---
+++ test/lottery.test.js
@@ -0,0 +1,34 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3'); //constructor function
+const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
+
+let accounts;
+let lottery;
+
+beforeEach(async () => {
+    //get a list of all accounts
+    accounts = await web3.eth.getAccounts();
+    //use one of them to deploy the contract
+    lottery = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode })
+        .send({ from: accounts[0], gas: '1000000' })
+}); 
+
+describe('Lottery Contract', () => {
+    it('Deploy a contract', () => {
+        assert.ok(lottery.options.address);
+    });
+    it('Can enter the lottery', async () => {
+        await lottery.methods.enter().send({ 
+            from: accounts[0], 
+            value: web3.utils.toWei('0.02', 'ether')
+        });
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+        });
+        assert.strictEqual(accounts[0], players[0]);
+        assert.strictEqual(1, players.length);
+    })
+})
