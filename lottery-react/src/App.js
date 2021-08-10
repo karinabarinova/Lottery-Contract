ec8137582e0018d6af98185125160c9069e82b49
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-08-10 22:13:39 +0300

    Accessing more properties

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -6,11 +6,16 @@ import lottery from './lottery';
 
 function App() {
   const [manager, setManager] = useState('');
-  
+  const [players, setPlayers] = useState([]);
+  const [balance, setBalance] = useState('0');
   useEffect(() => {
     async function fetchManager() {
       const response = await lottery.methods.manager().call();
+      const players = await lottery.methods.getPlayers().call();
+      const balance = await web3.eth.getBalance(lottery.options.address);
       setManager(response);
+      setPlayers(players);
+      setBalance(balance);
     }
     fetchManager()
   }, []);
@@ -20,6 +25,9 @@ function App() {
     <div>
       <h2>Lottery Contracts</h2>
       <p>This contract is managed by: {manager}</p>
+      <p>There are currently {players.length} people entered, 
+      competing to win {web3.utils.fromWei(balance, 'ether')} ether!</p>
+      <h3>Want to try your luck?</h3>
     </div>
   );
 }
