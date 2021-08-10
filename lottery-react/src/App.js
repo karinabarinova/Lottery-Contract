5a2d9d0a67162559bfbb9929024823ac9d7c7187
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-08-10 18:26:24 +0300

    rendering contract data

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -1,24 +1,25 @@
+import React, { useEffect, useState } from 'react'; 
 import logo from './logo.svg';
 import './App.css';
 import web3 from './web3';
+import lottery from './lottery';
 
 function App() {
+  const [manager, setManager] = useState('');
+  
+  useEffect(() => {
+    async function fetchManager() {
+      const response = await lottery.methods.manager().call();
+      setManager(response);
+    }
+    fetchManager()
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Lottery Contracts</h2>
+      <p>This contract is managed by: {manager}</p>
     </div>
   );
 }
